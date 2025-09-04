26f2ac6c24637c1726d802324031c2b10ac0c29f
Osmar Romero <osmarromero335@gmail.com>
2025-09-04 15:55:36 -0600

    Refactor: configurando de manera correcta el servidor

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import express from 'express'
+import { config } from "dotenv";
+import { initServer } from "./configs/app.js";
 
-const app = express()
-
-app.get('/', (req, res) => {
-  res.status(202).json({
-    msg: "Hola Mundo"
-  })
-  console.log("Dentro del get")
-})
-
-app.listen(3000)
+config();
+initServer();
