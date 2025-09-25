19baca38b9c161b718376c6434d72638211c9aa0
Osmar Romero <osmarromero335@gmail.com>
2025-09-25 15:40:49 -0600

    25/09/2025

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../users/user.model.js'
-import {hash} from 'argon2'
+import { hash, verify } from 'argon2'
+import { generarJWT } from '../../helpers/JWS-generate.js'
+import { token } from 'morgan'
 
 export const register = async (req, res) => {
     try{
@@ -16,6 +18,13 @@ export const register = async (req, res) => {
             password: encryptedPassword,
             profilePicture
         })
+        return res.status(200).json({
+            message: "Usuario registrado correctamente",
+            userDetails: {
+                user: newUser.username,
+                email: newUser.email,
+            },
+        });
     }catch(error){
         return res.status(500).json({
             message: 'Error al registrar el usuario',
@@ -23,3 +32,33 @@ export const register = async (req, res) => {
         })
     }
 }
+
+export const login = async (req, res) =>{
+    const {email, password, username} = req.body;
+
+    try {
+        const lowerEmail = email ? email.toLowerCase() : null;
+        const lowerUsername = username ? username.toLowerCase() : null;
+
+        const user = await User.findOne({
+            $or: [{ email: lowerEmail}, { username: lowerUsername}],
+        });
+    if (!user) {
+        return res.status(200).json({
+            message: "Inicio de sesión exitoso",
+            userDetails: {
+
+                    username: username,
+                    token: token,
+                    profilePicture: user.profilePicture,
+                    uid: user.id
+                }
+        });
+    }
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Error del servidor',
+        error: error.message,
+      });
+    }
+};
