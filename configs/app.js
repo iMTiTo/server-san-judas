fff22cf3b8e2e4b1620da61c5f94a0f5e32cea66
MattC78 <mchavarria-s017t@kinal.edu.gt@gmail.com>
2026-08-11 15:38:48 -0600

    11/08/2026

--- configs/app.js
+++ configs/app.js
@@ -23,7 +23,7 @@ const middlewares = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(cors({
-        origin: '*',
+        origin: ['https://recetariosjt.web.app', 'http://localhost:5173', 'http://localhost:3000'],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization']
